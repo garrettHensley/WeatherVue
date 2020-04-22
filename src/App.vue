4ea2804fb7fc9e8f15fc609c52fcbcd5df5df824
garrettmichael <garesnap@gmail.com>
2020-04-22 15:55:25 -0400

    Commit on master

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav" class="container lead">
-      <router-link to="/">Home</router-link>
+    <div id="nav" class="lead bg-dark text-center text-light">
+      <router-link to="/" class="text-light ">Home</router-link>
     </div>
     <router-view />
+
+    <div class="container fixed-bottom text-center">
+      <span class="text-muted">Created by Garrett Hensley</span>
+    </div>
   </div>
 </template>
+
+<style>
+body {
+  background-color: #35413f;
+}
+</style>
